79550dcc0984bb8e11906eeb6cd5c92c7d0b7d4d
Darref <jordan.ferrad@gmail.com>
2023-11-08 11:09:46 +0100

    calculette done ! all

--- React_3/TP-calculatrice/Tp-calculette/src/App.tsx
+++ React_3/TP-calculatrice/Tp-calculette/src/App.tsx
@@ -11,11 +11,27 @@ function App() {
       setChaineResultat(newResult);
       },[chaineResultat])
 
+  const handleClearing = useCallback((val:string)=>{
+    console.log(val , chaineResultat)
+    let newResult = chaineResultat.substring(0 , chaineResultat.length -1)
+    setChaineResultat(newResult);
+    },[chaineResultat])
+
+  const handleEqual = useCallback((val:string)=>{
+    console.log(val , chaineResultat)
+    //
+    
+    //
+    let newResult = evaluateExpression(chaineResultat)
+    setChaineResultat(newResult.toString());
+    console.log("ici:" , newResult)
+    },[chaineResultat])
+
   return (
     <div className='Calc'>
       <div className='zoneResultat'>
         <p className='screenResultat'>{chaineResultat}</p>
-        <Bouton val={'<-'} returnValue={handleReturnValue}></Bouton>
+        <Bouton val={'<-'} returnValue={handleClearing}></Bouton>
       </div>
       <div className='zonePad'>
         <div className='zoneBoutonsNombres'>
@@ -33,7 +49,9 @@ function App() {
         <div className='zoneBoutonsOperateurs'>
           <Bouton val={'+'} returnValue={handleReturnValue}></Bouton>
           <Bouton val={'-'} returnValue={handleReturnValue}></Bouton>
-          <Bouton val={'='} returnValue={handleReturnValue}></Bouton>
+          <Bouton val={'*'} returnValue={handleReturnValue}></Bouton>
+          <Bouton val={'/'} returnValue={handleReturnValue}></Bouton>
+          <Bouton val={'='} returnValue={handleEqual}></Bouton>
         </div>
       </div>
     </div>
@@ -41,3 +59,37 @@ function App() {
 }
 
 export default App
+
+function evaluateExpression(expression: string) {
+// Utilisez la méthode split pour diviser l'expression en termes
+const terms = expression.split(/([+\-*/])/).map((term) => term.trim());
+
+// Convertissez les termes en nombres et effectuez les calculs
+let result = parseFloat(terms[0]);
+
+for (let i = 1; i < terms.length; i += 2) {
+  const operator = terms[i];
+  const operand = parseFloat(terms[i + 1]);
+
+  switch (operator) {
+    case "+":
+      result += operand;
+      break;
+    case "-":
+      result -= operand;
+      break;
+    case "*":
+      result *= operand;
+      break;
+    case "/":
+      if (operand === 0) {
+        throw new Error("Division par zéro");
+      }
+      result /= operand;
+      break;
+    default:
+      throw new Error("Opérateur non reconnu");
+    }
+  }
+  return result;
+}
